83d73ef02b6343859fbe87502e1f41b5129c71c6
Nikhilesh Singh <nikhilesh2010@live.com>
2018-02-24 16:37:35 -0500

    finished landing page

--- app/containers/SearchPage/index.js
+++ app/containers/SearchPage/index.js
@@ -177,8 +177,8 @@ class SearchPage extends Component {
           <div id={styles.main}>
             <div className={styles.inner}>
               <header>
-                <h1>This is Phantom, a free, fully responsive site<br />
-                template designed by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                <h1>Beer me a pet<br />
+                 <a href="http://html5up.net">HTML5 UP</a>.</h1>
                 <p>Etiam quis viverra lorem, in semper lorem. Sed nisl arcu euismod sit amet nisi euismod sed cursus arcu elementum ipsum arcu vivamus quis venenatis orci lorem ipsum et magna feugiat veroeros aliquam. Lorem ipsum dolor sit amet nullam dolore.</p>
               </header>
               <section className={styles.tiles}>
